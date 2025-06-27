6551efdddf71732290e3c91eee8818979ca0aa92
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-06-27 13:00:51 +0800

    Create script for data-tables

--- private/assets/js/data-tables.js
+++ private/assets/js/data-tables.js
@@ -1,45 +1,30 @@
-// Script for login
-$("#login-button").click(() => {
-  const username = $("#username").val();
-  const password = $("#password").val();
-
-  if (username.trim() === "") {
-    Swal.fire({
-      text: "Username is required.",
-      icon: "warning",
-    });
-  } else if (password.trim() === "") {
-    Swal.fire({
-      text: "Password is required.",
-      icon: "warning",
-    });
-  } else {
-    $.ajax({
-      url: "./login_process.php",
-      method: "POST",
-      data: {
-        username: username,
-        password: password,
-      },
-      success: (response) => {
-        if (response == "Incorrect username or password.") {
-          Swal.fire({
-            text: "Incorrect username or password",
-            icon: "error",
-          });
-        } else {
-          Swal.fire({
-            text: "Access Granted",
-            icon: "success",
-          }).then(() => {
-            if (response == "admin") {
-              location.href = "./admin/";
-            } else if (response == "moderator") {
-              location.href = "./moderator";
-            }
-          });
-        }
-      },
-    });
-  }
+$(".data-table").DataTable({
+  layout: {
+    topStart: {
+      buttons: [
+        {
+          extend: "excel",
+        },
+        {
+          extend: "colvis",
+          columns: ':not(:first-child)',
+        },
+      ],
+    },
+  },
+  columnDefs: [
+    {
+      className: "dtr-control",
+      orderable: false,
+      target: 0,
+      visible: true,
+    },
+  ],
+  order: [1, "asc"],
+  responsive: {
+    details: {
+      type: "column",
+      target: "tr",
+    },
+  },
 });
