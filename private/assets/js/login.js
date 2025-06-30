9e0f49db7e84323a6a13bb925737dcac73298489
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-06-30 20:51:12 +0800

    Use single quotation

--- private/assets/js/login.js
+++ private/assets/js/login.js
@@ -1,44 +1,46 @@
-$("#login-button").click(() => {
-  const username = $("#username").val();
-  const password = $("#password").val();
+$(function () {
+  $('#login-btn').click(() => {
+    const username = $('#username').val().trim();
+    const password = $('#password').val().trim();
 
-  if (username.trim() === "") {
-    Swal.fire({
-      text: "Username is required.",
-      icon: "warning",
-    });
-  } else if (password.trim() === "") {
-    Swal.fire({
-      text: "Password is required.",
-      icon: "warning",
-    });
-  } else {
-    $.ajax({
-      url: "./login_process.php",
-      method: "POST",
-      data: {
-        username: username,
-        password: password,
-      },
-      success: (response) => {
-        if (response == "Incorrect username or password.") {
-          Swal.fire({
-            text: "Incorrect username or password",
-            icon: "error",
-          });
-        } else {
-          Swal.fire({
-            text: "Access Granted",
-            icon: "success",
-          }).then(() => {
-            if (response == "admin") {
-              location.href = "./admin/";
-            } else if (response == "moderator") {
-              location.href = "./moderator";
-            }
-          });
-        }
-      },
-    });
-  }
+    if (username === '') {
+      Swal.fire({
+        text: 'Username is required.',
+        icon: 'warning',
+      });
+    } else if (password === '') {
+      Swal.fire({
+        text: 'Password is required.',
+        icon: 'warning',
+      });
+    } else {
+      $.ajax({
+        url: './login_process.php',
+        method: 'POST',
+        data: {
+          username: username,
+          password: password,
+        },
+        success: (response) => {
+          if (response === 'Incorrect username or password.') {
+            Swal.fire({
+              text: 'Incorrect username or password',
+              icon: 'error',
+            });
+          } else {
+            Swal.fire({
+              text: 'Access Granted',
+              icon: 'success',
+            }).then(() => {
+              if (response === 'admin') {
+                location.href = './admin/';
+              } else if (response === 'moderator') {
+                location.href = './moderator';
+              }
+            });
+          }
+        },
+      });
+    }
+  });
 });
