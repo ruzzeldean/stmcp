df31a024ed1a12af4b4e03dc535357a9292834d3
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-07-11 23:09:40 +0800

    Create reusable file for datatables include

---
+++ private/includes/datatables/scripts_include.php
@@ -0,0 +1,11 @@
+  <script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script>
+  <script src="https://cdn.datatables.net/2.3.2/js/dataTables.bootstrap4.js"></script>
+  <!-- DataTables Responsive -->
+  <script src="https://cdn.datatables.net/responsive/3.0.4/js/dataTables.responsive.js"></script>
+  <script src="https://cdn.datatables.net/responsive/3.0.4/js/responsive.bootstrap4.js"></script>
+  <!-- DataTables Buttons -->
+  <script src="https://cdn.datatables.net/buttons/3.2.3/js/dataTables.buttons.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.bootstrap4.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.colVis.min.js"></script>
---
+++ private/includes/datatables/styles_include.php
@@ -0,0 +1,3 @@
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.4/css/responsive.bootstrap4.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.3/css/buttons.bootstrap4.css">
