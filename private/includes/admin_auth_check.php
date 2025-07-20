77d468bfbfd0aa530276ab9d4f3de04d51d12715
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-07-20 19:20:56 +0800

    refactor

--- private/includes/admin_auth_check.php
+++ private/includes/admin_auth_check.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
   session_start();
 }
 
-if (!isset($_SESSION['adminGatepass']) || $_SESSION['roleID'] === '2') {
+if (!isset($_SESSION['adminGatepass'])) {
   header('Location: /stmcp/private/');
   exit;
 }
