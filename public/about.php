be7b9ad05512d67282ac51cf30124482e5c66dfe
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-07-24 00:02:03 +0800

    feat: add sections

--- public/about.php
+++ public/about.php
@@ -5,8 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Star Touring Motorcycle Club Philippines</title>
   <link rel="shortcut icon" href="./assets/img/logo/logo.png" type="image/x-icon">
+  <!-- Font Awesome Icon -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Bootstrap 5 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
   <!-- Custom CSS -->
   <link rel="stylesheet" href="./assets/css/style.css">
 </head>
@@ -35,7 +37,7 @@
             <div class="offcanvas-body pe-5 pe-lg-0">
               <ul class="navbar-nav justify-content-end flex-grow-1">
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="./">Home</a>
+                  <a class="nav-link" href="./">Home</a>
                 </li> <!-- End of .nav-item (home) -->
                 
                 <li class="nav-item">
@@ -62,72 +64,156 @@
       <!-- Hero section -->
       <section id="home-hero" class="hero d-flex justify-content-center align-items-center py-5">
         <div class="container py-5">
-          <div class="row">
-            <div class="d-flex justify-content-center align-items-center mt-5">
-              <div id="hero-content" class="text-center">
-                <h1 class="display-3 fw-semibold">ABOUT US</h1>
+          <div class="row py-5 px-3">
+            <div class="border border-5 border-warning rounded d-flex justify-content-center align-items-center mt-5 py-5">
+              <div id="hero-content" class="text-center py-5">
+                <h1 class="display-1 fw-semibold">ABOUT US</h1>
               </div> <!-- End of #hero-content -->
             </div> <!-- End of .col -->
           </div> <!-- End of .row -->
         </div> <!-- End of .container -->
       </section> <!-- End of hero-section -->
 
-      <!-- History section (header) -->
-      <section class="py-5">
-        <div class="container">
-          <h2 class="text-center display-5 fw-semibold mb-3">Our History</h2>
-          <p class="lead text-muted text-center fw-normal">The journey of Star Touring Motorcycle Club Philippines (STMCP) — from humble beginnings to a strong community committed to charity and riding adventures.</p>
-        </div> <!-- End of .container -->
-      </section> <!-- End of history section (header) -->
+      <!-- Our History -->
+      <section class="bg-body-tertiary px-3 py-5">
+        <div class="container py-5">
+          <h2 class="text-center display-4 fw-semibold mb-3">Our History</h2>
+          <p class="lead text-muted">The journey of Star Touring Motorcycle Club Philippines (STMCP) — from humble beginnings to a strong community committed to charity and riding adventures.</p>
+        </div>
+      </section>
 
-      <!-- Foundation section -->
-      <section class="py-5">
-        <div class="container">
+      <!-- Foundation -->
+      <section class="px-3 py-5">
+        <div class="container pt-3">
           <div class="mb-4">
-            <h4 class="display-6 fw-semibold text-center">Foundation</h4>
-          </div> <!-- End of div -->
+            <h3 class="display-5 fw-semibold text-center">Foundation</h3>
+          </div>
 
-          <div class="row row-gap-4">
-            <div class="col-lg-6">
-              <div id="four-pillar-img" class="rounded"></div>
-            </div> <!-- End of .col for 4 pillars img -->
-
-            <div class="col-lg-6 lead d-flex flex-column justify-content-center">
+          <div class="row row-gap-3">
+            <div class="col-lg-6 d-flex align-items-center">
               <div>
+                <img class="img-fluid rounded" src="./assets/img/four-pillars/four-pillars.jpg" alt="Four pillars">
+              </div>
+            </div>
+
+            <div class="col-lg-6 card shadow">
+              <div class="lead p-3 h-100 d-flex flex-column justify-content-center">
                 <p>The Star Touring Motorcycle Club Philippines (STMCP) was founded by four key individuals, affectionately known within the club as the "4 Pillars"—Erick Lee, Alo Yap, Ericson Gallito, and Mike Miranda.</p>
               
                 <p>On July 5, 2021, STMCP was officially registered with the Securities and Exchange Commission (SEC), marking its formal recognition as an organization.</p>
-              </div> <!-- End of div -->
-            </div> <!-- End of .col -->
-          </div> <!-- End of .row -->
-        </div> <!-- End of .container -->
-      </section> <!-- End of foundation section -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-      <!-- Growth & expansion section -->
-      <section class="py-5">
-        <div class="container">
+      <!-- Growth & Expansion -->
+      <section class="px-3 py-5">
+        <div class="container pb-3">
           <div class="mb-4">
-            <h4 class="display-6 fw-semibold text-center">Growth & Expansion</h4>
-          </div> <!-- End of div -->
+            <h3 class="display-5 fw-semibold text-center">Growth & Expansion</h3>
+          </div>
 
-          <div class="row row-gap-4">
-
-            <div class="col-lg-6">
-              <div id="four-pillar-img" class="order-lg-last rounded"></div>
+          <div class="row row-gap-3">
+            <div class="col-lg-6 order-lg-last d-flex align-items-center">
+              <div>
+                <img class="img-fluid rounded" src="./assets/img/about-page/growth-and-expansion/growth_and_expansion.png" alt="Four pillar">
+              </div>
             </div>
 
-            <div class="col-lg-6 order-lg-first d-flex flex-column justify-content-center">
-              <div class="lead ">
+            <div class="col-lg-6 order-lg-first card shadow">
+              <div class="lead p-3 h-100 d-flex flex-column justify-content-center">
                 <p>Since its inception, STMCP has steadily grown, attracting motorcycle enthusiasts from the Luzon region and beyond.</p>
 
                 <p>The club has expanded through word of mouth and active community involvement, creating a network of passionate riders.</p>
 
                 <p>Today, STMCP is recognized not only as a riding club but as a vibrant and growing community united by shared values and advocacy.</p>
               </div>
-            </div> <!-- End of .col -->
-          </div> <!-- End of .row -->
-        </div> <!-- End of .container -->
-      </section> <!-- End of growth & expansion section -->
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Advocacy & Events -->
+      <section class="bg-body-tertiary px-3 py-5">
+        <div class="container py-3">
+          <div class="mb-4">
+            <h3 class="display-5 fw-semibold text-center">Advocacy & Events</h3>
+          </div>
+
+          <div class="row row-gap-4">
+            <div class="col-lg-6">
+              <div class="lead rounded shadow p-4">
+                <p>STMCP is not only about riding motorcycles—it also has a strong commitment to charity work. The club organizes charity rides and outreach programs to help those in need, reflecting their mission of giving back to the community. Their goal is to foster brotherhood, adventure, and social responsibility among members.</p>
+              </div>
+            </div>
+
+            <div class="col-lg-6">
+              <div class="lead rounded shadow p-4 h-100">
+                <p>To become an official member of STMCP, an applicant must meet the following requirements:</p>
+
+                <ul>
+                  <li>Attend one eyeball (EB) gathering</li>
+                  <li>Participate in one charity event</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Current Status & Future Look -->
+      <section class="px-3 py-5">
+        <div class="container py-3">
+          <h3 class="text-center display-5 fw-semibold mb-4">Current Status & Future Look</h3>
+
+          <p class="lead">As of today, STMCP has grown to an estimated 1,500 members, and the club continues to expand. With its strong foundation, mission-driven activities, and ever-growing community, STMCP is expected to continue its legacy of uniting motorcycle enthusiasts while making a positive impact on society.</p>
+        </div>
+      </section>
+
+      <!-- Mission Vision -->
+      <section class="bg-body-tertiary px-3 py-5">
+        <div class="container py-3">
+          <div class="mb-4">
+            <h3 class="display-5 fw-semibold text-center">Mission & Vision</h3>
+          </div>
+
+          <div class="row row-gap-4">
+            <div class="col-md-6">
+              <div class="lead card shadow p-4 h-100">
+                <i class="fa-solid fa-bullseye mb-4 text-center text-warning fs-1"></i>
+
+                <h4 class="text-center mb-3">Mission</h4>
+
+                <p>Our mission is to build a strong community among all riders and create a Big Star Family founded on camaraderie, unity, and mutual respect. We promote touring across the Philippines, encouraging riders to experience unforgettable journeys together.</p>
+
+                <p>STMCP and its local chapters are also committed to giving back through charitable initiatives. We actively support and participate in various motorcycle events and activities that pursue, promote, and protect the interests of motorcyclists.</p>
+
+                <p>In addition, STMCP advocates for motorcycle and road safety to ensure the well-being of all members of the STMCP family and to foster positive relations with the general public.</p>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="lead card shadow p-4 h-100">
+                <i class="fa-solid fa-eye mb-4 text-center text-warning fs-1"></i>
+
+                <h4 class="text-center mb-3">Vision</h4>
+
+                <p>Our long-term goal is to create members for life. We will earn our members' loyalty by listening to them, anticipating their needs, and taking action to create value in their eyes.</p>
+                
+                <p>We strive to be known for our reliability, flexibility, and responsiveness. Growth, longevity, and member satisfaction will naturally follow.</p>
+
+                <p>We are committed to empowering our members in an enriching environment built on trust, cooperation, and mutual respect.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Core Values -->
+      <section>
+    
+      </section>
     </main>
     <!-- End of main -->
     
@@ -137,6 +223,7 @@
   </div>
   <!-- End of .wrapper -->
   
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  <!-- Bootstrap 5 -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
 </html>
