f61e247d24578236899ec2d67bada2094d15f742
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-08-18 23:52:37 +0800

    refactor(layout): extract head, aside(nav), and scripts into reusable components

--- private/moderator/pages/index.php
+++ private/moderator/pages/index.php
@@ -3,124 +3,13 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Moderator | Star Touring Motorcycle Club</title>
-  <link rel="shortcut icon" href="../../assets/shared/images/logo/logo.png" type="image/x-icon">
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- AdminLTE 4 -->
-  <link rel="stylesheet" href="../../assets/shared/css/adminlte.min.css">
+  <?php require_once __DIR__ . '/../../includes/moderator/head.php'; ?>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
   <div class="wrapper">
 
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-      </ul>
-    </nav>
-    <!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="./" class="brand-link">
-        <img src="../../assets/shared/images/logo/logo.png" alt="STMCP Logo" class="brand-image">
-        <span class="brand-text font-weight-light">MODERATOR | STMCP</span>
-      </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            <img src="../../assets/shared/images/user-icon/user-icon.png" alt="User Icon">
-          </div>
-          <div class="info">
-            <a href="#" class="d-block"><?php echo e(strtoupper($_SESSION['firstName'])); ?></a>
-          </div>
-        </div>
-
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li class="nav-header">MAIN</li>
-
-            <li class="nav-item">
-              <a href="./" class="nav-link active">
-                <i class="nav-icon fa-solid fa-gauge-high"></i>
-                <p>
-                  Dashboard
-                </p>
-              </a>
-            </li>
-
-            <li class="nav-header">MEMBERS</li>
-
-            <li class="nav-item">
-              <a href="./aspirants.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-user-check"></i>
-                <p>
-                  Aspirants
-                </p>
-              </a>
-            </li>
-
-            <li class="nav-header">CONTENT MANAGEMENT</li>
-
-            <li class="nav-item">
-              <a href="./posts.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-newspaper"></i>
-                <p>Posts</p>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a href="./donations.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-coins"></i>
-                <p>Donations</p>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a href="./donations.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-tags"></i>
-                <p>Merchandise</p>
-              </a>
-            </li>
-
-            <li class="nav-header">SYSTEM</li>
-
-            <li class="nav-item">
-              <a href="./settings.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-gear"></i>
-                <p>Settings</p>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a href="../../" class="nav-link">
-                <i class="nav-icon fa-solid fa-arrow-right-from-bracket"></i>
-                <p>Logout</p>
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
-    </aside>
+    <?php require_once __DIR__ . '/../../includes/moderator/aside.php'; ?>
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -191,14 +80,7 @@
   </div>
   <!-- ./wrapper -->
 
-  <!-- REQUIRED SCRIPTS -->
-
-  <!-- jQuery -->
-  <script src="../../assets/shared/js/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="../../assets/shared/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="../../assets/shared/js/adminlte.min.js"></script>
+  <?php require_once __DIR__ . '/../../includes/moderator/scripts.php' ?>
 </body>
 
 </html>
--- private/moderator/pages/posts.php
+++ private/moderator/pages/posts.php
@@ -11,128 +11,13 @@ $csrfToken = $_SESSION['csrfToken'];
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Moderator | Star Touring Motorcycle Club</title>
-  <link rel="shortcut icon" href="../../assets/shared/images/logo/logo.png" type="image/x-icon">
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- AdminLTE 4 -->
-  <link rel="stylesheet" href="../../assets/shared/css/adminlte.min.css">
-  <!-- DataTables -->
-  <?php require_once __DIR__ . '/../../includes/datatables/styles_include.php'; ?>
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="../../assets/shared/css/style.css">
+  <?php require_once __DIR__ . '/../../includes/moderator/head.php'; ?>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
   <div class="wrapper">
 
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-      </ul>
-    </nav>
-    <!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="./" class="brand-link">
-        <img src="../../assets/shared/images/logo/logo.png" alt="STMCP Logo" class="brand-image">
-        <span class="brand-text font-weight-light">MODERATOR | STMCP</span>
-      </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-            <img src="../../assets/shared/images/user-icon/user-icon.png" alt="User Icon">
-          </div>
-          <div class="info">
-            <a href="#" class="d-block"><?php echo e(strtoupper($_SESSION['firstName'])); ?></a>
-          </div>
-        </div>
-
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li class="nav-header">MAIN</li>
-
-            <li class="nav-item">
-              <a href="./" class="nav-link">
-                <i class="nav-icon fa-solid fa-gauge-high"></i>
-                <p>
-                  Dashboard
-                </p>
-              </a>
-            </li>
-
-            <li class="nav-header">MEMBERS</li>
-
-            <li class="nav-item">
-              <a href="./aspirants.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-user-check"></i>
-                <p>
-                  Aspirants
-                </p>
-              </a>
-            </li>
-
-            <li class="nav-header">CONTENT MANAGEMENT</li>
-
-            <li class="nav-item">
-              <a href="./posts.php" class="nav-link active">
-                <i class="nav-icon fa-solid fa-newspaper"></i>
-                <p>Posts</p>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a href="./donations.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-coins"></i>
-                <p>Donations</p>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a href="./donations.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-tags"></i>
-                <p>Merchandise</p>
-              </a>
-            </li>
-
-            <li class="nav-header">SYSTEM</li>
-
-            <li class="nav-item">
-              <a href="./settings.php" class="nav-link">
-                <i class="nav-icon fa-solid fa-gear"></i>
-                <p>Settings</p>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a href="../../" class="nav-link">
-                <i class="nav-icon fa-solid fa-arrow-right-from-bracket"></i>
-                <p>Logout</p>
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-      </div>
-      <!-- /.sidebar -->
-    </aside>
+    <?php require_once __DIR__ . '/../../includes/moderator/aside.php'; ?>
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -157,8 +42,8 @@ $csrfToken = $_SESSION['csrfToken'];
       <div class="content">
         <div class="container-fluid">
           <div class="row mb-3">
-            <a class="btn btn-app bg-primary" href="create_post.php">
-              <i class="fas fa-square-plus"></i> Create
+            <a class="btn btn-app bg-primary pb-5" href="create_post.php">
+              <i class="fa-solid fa-square-plus"></i> Create
             </a>
           </div> <!-- /.row -->
 
@@ -286,21 +171,7 @@ $csrfToken = $_SESSION['csrfToken'];
   </div>
   <!-- ./wrapper -->
 
-  <!-- REQUIRED SCRIPTS -->
-
-  <!-- jQuery -->
-  <script src="../../assets/shared/js/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="../../assets/shared/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="../../assets/shared/js/adminlte.min.js"></script>
-  <!-- DataTables -->
-  <?php require_once __DIR__ . '/../../includes/datatables/scripts_include.php' ?>
-  <!-- SweetAlert2 -->
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <!-- Custom script -->
-  <script src="../../assets/shared/js/data_tables.js"></script>
-  <script src="../../assets/shared/js/shared.js"></script>
+  <?php require_once __DIR__ . '/../../includes/moderator/scripts.php'; ?>
   <script src="../../assets/moderator/js/posts/preview_post.js"></script>
   <script src="../../assets/moderator/js/posts/delete_post.js"></script>
 </body>
