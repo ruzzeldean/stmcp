45cf4a11448c9b64b3a4a1f13c7d18868250537c
ruzzeldean <ruzzelparungao.xyz@gmail.com>
2025-10-12 22:22:03 +0800

    feat: add button & modal for searching other users

--- private/admin/pages/contacts.php
+++ private/admin/pages/contacts.php
@@ -12,7 +12,7 @@
     <?php require_once __DIR__ . '/../../includes/admin/aside.php'; ?>
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+    <div id="contacts-content-wrapper" class="content-wrapper">
       <!-- Content Header (Page header) -->
       <div class="content-header">
         <div class="container-fluid">
@@ -30,8 +30,8 @@
         <div class="container-fluid">
           <div class="row card overflow-hidden">
             <div class="card-header border-bottom-0 px-1">
-              <div id="search-container" class="col-12">
-                <input id="search-input" class="form-control" type="search" placeholder="Search contacts">
+              <div class="search-container col-12">
+                <input id="search-contacts" class="search-input form-control" type="search" placeholder="Search contacts">
                 <i id="search-icon" class="fa-solid fa-magnifying-glass"></i>
               </div>
             </div> <!-- /.card-header -->
@@ -43,6 +43,38 @@
         </div><!-- /.container-fluid -->
       </div>
       <!-- /.content -->
+
+      <button id="add-contact" class="btn btn-warning rounded-circle" type="button" data-toggle="modal" data-target="#add-contact-modal" title="Add a contact">
+        <i class="fa-solid fa-plus"></i>
+      </button>
+
+      <div id="add-contact-modal" class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="modal-label" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 id="modal-label" class="modal-title">Add New Contact</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+            <div class="modal-body pb-0">
+              <div class="card border-0 shadow-none">
+                <div class="card-header p-2">
+                  <div class="search-container col-12">
+                    <input id="search-new-contacts" class="search-input form-control" type="search" placeholder="Search new contact">
+                    <i id="search-new-icon" class="fa-solid fa-magnifying-glass"></i>
+                  </div>
+                </div>
+
+                <div id="new-contact-results" class="card-body">
+                  <!-- result goes here -->
+                </div>
+              </div> <!-- /.card -->
+            </div>
+          </div>
+        </div>
+      </div> <!-- /#add-contact-modal -->
     </div>
     <!-- /.content-wrapper -->
 
